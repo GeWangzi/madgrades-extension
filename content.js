6c805129260388a5474c2fb689bb926b2e5bc76a
BrianOuyangLe <Brian.Ouyang.Le@gmail.com>
2025-02-14 21:53:27 -0800

    content.js now gets H2 header with CourseName
    - send the course name back to popup.js
    (via storage or message)
    - connect to pullGrade();

--- content.js
+++ content.js
@@ -1,1 +1,22 @@
 //Usefull
+console.log("Hi from console!")
+
+// content.js
+const observer = new MutationObserver(() => {
+    console.log(document.documentElement.outerHTML);
+
+    const element = document.querySelector("h2");
+    if (element) {
+        console.log("Text Content:", element.textContent);
+    }
+    else {
+        console.log("Element not found.");
+    }
+
+  });
+  
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  });
+  
